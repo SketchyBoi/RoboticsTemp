35d98b97271b34d76b2189327634004ba46af39d
SketchyBoi <whirlwindfury21@gmail.com>
2022-10-25 16:04:25 -0700

    Inheritance happened dont yell at me plz

---
+++ Week 3/Inheritance/Inheritance.java
@@ -0,0 +1,17 @@
+public class Inheritance {
+    
+}
+class Parent
+{
+    String last_name;
+    String eye_color;
+    public Parent(String last_name, String eye_color)
+    {
+        this.last_name = last_name;
+        this.eye_color = eye_color;
+    }
+    public String getLastName()
+    {
+        return last_name;
+    }
+}
